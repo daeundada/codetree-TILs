db13d52a39bf514dee9a13baec6dd27ba8231d8d
daeun Chang <146816939+daeundada@users.noreply.github.com>
2024-04-04 11:50:24 +0900

    Title: 실수 입력받아 계산 2, Time: 176ms, Memory: 11MB, Status: Passed - CodeTree

--- 240404/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
+++ 240404/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        float a = sc.nextFloat();
 
-        System.out.printf("%.2",a+1.5);
+        Scanner sc = new Scanner(System.in);
+        Double a = sc.nextDouble();
+
+        System.out.printf("%.2f",a+1.5);
     }
 }
