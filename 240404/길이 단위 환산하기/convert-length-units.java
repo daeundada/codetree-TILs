5fa5e475fc4d2ba844544c70e6c855aea9063cac
daeun Chang <146816939+daeundada@users.noreply.github.com>
2024-04-04 11:46:18 +0900

    Title: 길이 단위 환산하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240404/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double ft=30.48;
+        
+        Scanner sc = new Scanner(System.in);
+
+        double n = sc.nextDouble();
+
+        System.out.printf("%.1f",ft*n);
+    }
+}
