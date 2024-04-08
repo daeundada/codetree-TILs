dcb37d64303198dea2f407b4eea93d44c46a8fe2
daeun Chang <146816939+daeundada@users.noreply.github.com>
2024-04-08 11:46:27 +0900

    Title: 두 사람, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240408/두 사람/two-person.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        String b = sc.next();
+        int c = sc.nextInt();
+        String d = sc.next();
+
+        if(a>=19 || c>=19)
+		    System.out.print(1);    
+        else{
+            System.out.print(0);
+        }
+    }
+}
