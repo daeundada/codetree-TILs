177d3543372b45ff246b65df93cb3c19e050f3e7
daeun Chang <146816939+daeundada@users.noreply.github.com>
2024-04-08 11:06:58 +0900

    Title: 시험 통과 여부 확인하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240408/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a>=80){
+            System.out.print("pass");
+        }
+        else if(a<80){
+            System.out.printf("%d more score",80-a);
+        }        
+
+}
+}
