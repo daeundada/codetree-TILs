d0e121c31418233c18432909a2333790afb0e039
daeun Chang <146816939+daeundada@users.noreply.github.com>
2024-09-27 17:43:45 +0900

    Title: 그 계절, 그 날, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240927/그 계절, 그 날/that-season-that-day.java
+++ 240927/그 계절, 그 날/that-season-that-day.java
@@ -16,22 +16,20 @@ public class Main {
             if(D<=31){
                 month = M;
             }
-        }
-        if(M==4||M==6||M==9||M==11){
+        }else if(M==4||M==6||M==9||M==11){
             if(D<=30){
                 month = M;
             }
-        }
-        if(M==2){
+        }else if(M==2){
             if(Y%4==0){
-                if(Y%100==0 && Y%400==0 && D<=29){
-                    month = M;
-                }
-                if(Y%100==0 && D<=28){
-                    month = M;
-                }
-                if(D<=29){
-                    month = M;
+                if(Y%100==0 && Y%400!=0){
+                    if(D<=28){
+                        month = M;
+                    }
+                }else{
+                    if((D<=29)){
+                        month = M;
+                    }
                 }
             }else{
                 if(D<=28){
@@ -40,22 +38,16 @@ public class Main {
             }
         }
 
-        if(month>=3 && month<=5){
-            System.out.printf("Spring");
-        }
-        if(month>=6 && month<=8){
-            System.out.printf("Summer");
-        }
-        if(month>=9 && month<=11){
-            System.out.printf("Fall");
-        }
-        if(month>=12 || month<=2){
-            System.out.printf("Winter");
-        }
         if(month ==-1){
             System.out.printf("-1");
+        }else if(month>=3 && month<=5){
+            System.out.printf("Spring");
+        }else if(month>=6 && month<=8){
+            System.out.printf("Summer");
+        }else if(month>=9 && month<=11){
+            System.out.printf("Fall");
+        }else if(month>=12 || month<=2){
+            System.out.printf("Winter");
         }
-
-
     }
 }
